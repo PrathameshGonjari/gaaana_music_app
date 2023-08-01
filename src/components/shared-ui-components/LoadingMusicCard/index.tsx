93400f57b74d7261f63105367b3f972e1d641edf
Torinit-Prathamesh <104342622+Torinit-Prathamesh@users.noreply.github.com>
2023-08-01 13:54:33 +0530

    card loader style formated

--- src/components/shared-ui-components/LoadingMusicCard/index.tsx
+++ src/components/shared-ui-components/LoadingMusicCard/index.tsx
@@ -1,37 +1,30 @@
-
 import { Skeleton } from "@mui/material";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { Wrapper } from "../MusicCard/style";
+import { LoadingCardWrapper } from "./style";
 
 const LoadingMusicCard = () => {
+  const SkeletonWidth = ["100", "80", "60", "40"];
+
   return (
     <Wrapper>
       <Card sx={{ display: "flex" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            minWidth: "70%",
-            maxWidth: "70%",
-          }}
-          id="parent"
-        >
+        <LoadingCardWrapper>
           <CardContent sx={{ flex: "1 0 auto" }}>
-            <Skeleton variant="text" />
-            <Skeleton variant="text" width={"80%"} />
-            <Skeleton variant="text" width={"60%"} />
-            <Skeleton variant="text" width={"40%"} />
+            {SkeletonWidth?.map((width: string) => (
+              <Skeleton key={width} variant="text" width={`${width}%`} />
+            ))}
           </CardContent>
           <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
             <Skeleton variant="circular" width={38} height={38} />
           </Box>
-        </Box>
+        </LoadingCardWrapper>
         <Skeleton variant="rectangular" width={180} height={180} />
       </Card>
-    </Wrapper >
+    </Wrapper>
   );
-}
+};
 
-export default LoadingMusicCard
+export default LoadingMusicCard;
